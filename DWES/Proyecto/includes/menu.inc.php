b5bdd80a5e4db9f63aa38c2d6992c8e3e12c3821
adronmarsh <adrianmargil8@gmail.com>
2022-11-14 19:28:19 +0100

    UPDATED FILES - 14/11/2022

--- DWES/Proyecto/includes/menu.inc.php
+++ DWES/Proyecto/includes/menu.inc.php
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-<ul class="menu">
-    <li><img id="logo" src="media/revels_logo.png" alt="Logo of Revels"></li>
-    <li><a href="index.php"> Inicio</a></li>
-    <li><a href=""> Alta</a></li>
-    <li><a href=""> Juego</a></li>
-=======
-<ul class="menu">
-    <li><img id="logo" src="media/revels_logo.png" alt="Logo of Revels"></li>
-    <li><a href="index.php"> Inicio</a></li>
-    <li><a href=""> Alta</a></li>
-    <li><a href=""> Juego</a></li>
->>>>>>> 82c02f0f93e47263e49d6aa36affe5e726f076b0
-</ul>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Inicio</a></l>
+            <li><a href="">Inicio</a></li>
+        </ul>
+    </nav>
+</header>
