fc9ad86a6c38777a6802a6aef383db5fd13b1854
adronmarsh <adrianmargil8@gmail.com>
2022-11-22 14:43:26 +0100

    UPDATED FILES - 22/11/2022

--- DWES/Proyecto/includes/menu.inc.php
+++ DWES/Proyecto/includes/menu.inc.php
@@ -3,16 +3,23 @@
     <nav class="menu">
         <ul>
             <?php
+            if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+                // Windows
+                define('SLASH', '\\');
+            } else {
+                // Linux/Unix 
+                define('SLASH', '/');
+            }
             if (isset($_SESSION['usrSession'])) {
             ?>
-                <li><a href="index.php"><img src="../media/header/home.png" alt="home"></a></li>
-                <li><a href="new.php"><img src="../media/header/plus.png" alt="new"></a></li>
-                <li><a href="account.php"><img src="../media/header/user.png" alt="account"></a></li>
-                <li><a href="logout.php"><img src="../media/header/logout.png" alt="close"></a></li>
+                <li><a href="index.php"><img src="media/header/home.svg" alt="home"></a></li>
+                <li><a href="new.php"><img src="media/header/plus.svg" alt="new"></a></li>
+                <li><a href="account.php"><img src="media/header/user.svg" alt="account"></a></li>
+                <li><a href="logout.php"><img src="media/header/logout.svg" alt="close"></a></li>
             <?php
             } else {
             ?>
-                <li><a href="index.php"><img src="../media/header/home.png" alt="home"></a></li>
+                <li><a href="index.php"><img src="media/header/home.svg" alt="home"></a></li>
             <?php
             }
             ?>
