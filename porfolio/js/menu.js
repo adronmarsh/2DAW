2f7e6cd15e18d7dce30d500b7d0be5321b58fd75
adronmarsh <adrianmargil8@gmail.com>
2022-12-12 19:32:52 +0100

    UPDATED FILES - 12/12/2022

--- porfolio/js/menu.js
+++ porfolio/js/menu.js
@@ -4,7 +4,7 @@ class MyMenu extends HTMLElement {
         <nav class="sidebar">
             <ul class="menu">
                 <li><a href="index.html">inicio</a></li>
-                <li><a href="#!">sobre mi</a></li>
+                <li><a href="about.html">sobre mi</a></li>
                 <li><a href="skills.html">skills</a></li>
                 <li><a href="#!">proyectos</a></li>
                 <li><a href="#!">contacto</a></li>
