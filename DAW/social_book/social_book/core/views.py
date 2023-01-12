398b3150b654ec7b94380bae9cbfae4a17481045
adronmarsh <adrianmargil8@gmail.com>
2023-01-12 16:20:43 +0100

    UPDATED FILES - 12/01/2023

--- DAW/social_book/social_book/core/views.py
+++ DAW/social_book/social_book/core/views.py
@@ -8,53 +8,72 @@ from itertools import chain
 import random
 
 # Create your views here.
-
+# Si el usuario no ha iniciado sesión redirige a la página de "signin" 
 @login_required(login_url='signin')
 def index(request):
+    # Obtiene el perfil del usuario actual
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    # Crea una lista vacía para almacenar los usuarios que el usuario actual sigue
     user_following_list = []
+    # Crea una lista vacía para almacenar el contenido del feed
     feed = []
 
+    # Obtiene una lista de usuarios que el usuario actual sigue
     user_following = FollowersCount.objects.filter(follower=request.user.username)
 
+    # Añade los usuarios a la lista de seguimiento
     for users in user_following:
         user_following_list.append(users.user)
 
+    # Obtiene las publicaciones de los usuarios y las agrega a la feed
     for usernames in user_following_list:
         feed_lists = Post.objects.filter(user=usernames)
         feed.append(feed_lists)
 
+    # Combina todas las listas de feed en una sola lista
     feed_list = list(chain(*feed))
 
-    # user suggention starts
+    # Obtiene una lista de todos los usuarios en la base de datos
     all_users = User.objects.all()
+    
+    # Crea una lista vacía para almacenar usuarios que el usuario actual sigue
     user_following_all = []
 
+    # Obtiene los usuarios correspondientes
     for user in user_following:
         user_list = User.objects.get(username=user.user)
         user_following_all.append(user_list)
 
+    # Crea una lista de sugerencias donde aparecen usuarios que no son el usuario actual ni los usuarios a los que sigue
     new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
     current_user = User.objects.filter(username=request.user.username)
     final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
+    # Mezcla la lista de sugerencias de manera aleatoria
     random.shuffle(final_suggestions_list)
 
+    # Crea una lista vacía para almacenar los IDs de usuario de las sugerencias
     username_profile = []
+    # Crea una lista vacía para almacenar los perfiles de usuario de las sugerencias
     username_profile_list = []
 
+    # Obtiene los IDs de usuario y los añade a la lista
     for users in final_suggestions_list:
         username_profile.append(users.id)
 
+    # Obtiene los perfiles de los usuarios y los añade a la lista
     for ids in username_profile:
         profile_lists = Profile.objects.filter(id_user=ids)
         username_profile_list.append(profile_lists)
-    
+
+    # Combina todas las listas en una sola lista
     suggestions_username_profile_list = list(chain(*username_profile_list))
 
-    return render(request,'index.html', {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
-
+    # Devuelve los datos de 4 sugerencias.
+    return render(request,'index.html', {'user_profile': user_profile, 'posts': feed_list,
+                 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
+   
 @login_required(login_url='signin')
 def upload(request):
 
